61410368a0aa6135a0dfb827448d48397c56b480
Diego <diegoaquinde@gmail.com>
2022-03-02 13:58:47 -0500

    The home page was setup and we add code in index.php for make the home dynamis

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php get_header() ?>
 
+<?php if(have_posts()){ ?>
+    <?php while(have_posts()){ the_post();?>
+    <?php the_content(); ?>
+        <?php } ?>
+    <?php } ?>
 <main class="productos">
         <div class="container-fluid gx-5">
 
